a554c75736f97ee60eb22b70ee55dda99e33c810
clarkgh <demolisho@hotmail.com>
2017-07-29 08:50:06 -0700

    set plan for the day

--- timer.js
+++ timer.js
@@ -1,6 +1,14 @@
 const loadTime = Date.now(),
   endTime = loadTime + 11200000;
 
+  //set time in seconds
+  //set time in minutes
+  //serve remaining time in seconds
+  //set time in hours
+  //serve remaining time in minutes
+
+  //create another instance of the same timer
+
 var timerInterval = setInterval( function() {
   const currentTime = Date.now();
   
@@ -8,7 +16,6 @@ var timerInterval = setInterval( function() {
     seconds = ( timeDiff / 1000 ),
     minutes = ( seconds / 60 ),
     hours = ( minutes / 60 );
-    console.log(minutes)
   console.log( minutesLeft );
   if ( endTime < currentTime ) {
     clearInterval( timerInterval ); 
